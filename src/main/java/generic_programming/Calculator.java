85f43f435e266ce2365fc6760dc6d2de06a5e41e
Muxamedov Azamat <muxamedovazamat13@gmail.com git config --global user.email>
2024-09-20 20:53:22 +0500

    Initial commit

--- src/main/java/generic_programming/Calculator.java
+++ src/main/java/generic_programming/Calculator.java
@@ -1,4 +1,34 @@
 package generic_programming;
 
 public class Calculator {
+
+    private static <T extends Number> double sum(T a, T b) {
+        return a.doubleValue() + b.doubleValue();
+    }
+
+    private static <T extends Number> double multiply(T a, T b){
+        return a.doubleValue() * b.doubleValue();
+    }
+
+    private static <T extends Number> double divide(T a, T b){
+        return a.doubleValue() / b.doubleValue();
+    }
+
+    private static <T extends Number> double subtract(T a, T b){
+        return a.doubleValue() - b.doubleValue();
+    }
+
+    private static <T> boolean compareArrays(T[] a, T[] b){
+        if (a.length != b.length){
+            return false;
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                if (!a[i].equals(b[i])){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
 }
--- src/main/java/generic_programming/Pair.java
+++ src/main/java/generic_programming/Pair.java
@@ -1,4 +1,27 @@
 package generic_programming;
 
-public class Pair {
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
 }
